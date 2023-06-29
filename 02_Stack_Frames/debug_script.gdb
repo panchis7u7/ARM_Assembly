d9e5ede095e758df7c9b51a18398891bae34137d
panchis7u7 <smadrigal.rod@gmail.com>
2023-06-29 01:20:26 -0600

    Enhanced makefile to be architecture sensible.

---
+++ 02_Stack_Frames/debug_script.gdb
@@ -0,0 +1,3 @@
+break _start
+break func_1
+run
--- 02_Stack_Frames/x64_stack_frames.asm
+++ 02_Stack_Frames/x64_stack_frames.asm
@@ -7,15 +7,18 @@
 ; ld -o x64_stack_frames.out x64_stack_frames.o
 
 section .data
-    out_string: db "Hello World!",0xA,0
-    out_string_len: equ $-out_string
+    func_1_string: db "Function 1",0xA,0
+    func_1_string_len: equ $-func_1_string
+    func_2_string: db "Function 2",0xA,0
+    func_2_string_len: equ $-func_2_string_len
 
 section .text
 global _start
 
 _start:
 
-    call hello_world
+    mov rdi, 1          ; Pass a parameter to the func_1 function.
+    call func_1         ; Call the function.
 
     mov rax, 60
     mov rdi, 0x0
@@ -23,7 +26,7 @@ _start:
 
 ret
 
-hello_world:
+func_1:
     
     ; Function prologue.
     ; --------------------------------------------
@@ -31,11 +34,17 @@ hello_world:
     push rbp                    ; Save previous stack pointer to the stack.
     mov rbp, rsp                ; Move the current stack position (return address) to rbp.
 
+    sub rsp, 8                  ; Calculate the offset from RBP (frame pointer) for the parameter.
+    mov rax, rdi                ; Move the parameter value from the appropriate register to a general-purpose register (e.g., RAX).
+
+    mov qword [rbp-16], rax     ; Assuming parameter offset is -16 from RBP.
+
     mov rax, 1                  ; Use the write syscall.
     mov rdi, 1                  ; Use stdout (standard output stream) default 
                                 ; output channel that displays the output of a command or program.
-    mov rsi, out_string         ; Address of the string (char *)
-    mov rdx, out_string_len     ; Size of the displayed string.
+    mov rsi, func_1_string      ; Address of the string (char *)
+    mov rdx, func_1_string_len  ; Size of the displayed string.
+
     syscall
 
     ; Function epilogue.
@@ -45,3 +54,4 @@ hello_world:
             ; mov rsp,rbp
             ; pop rbp
     ret
+
